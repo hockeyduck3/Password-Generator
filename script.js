d411c946ed4f3459a534bf8f898aaca96b82f6bf
LJ Spencer <hockeyduck3@gmail.com>
2020-03-27 14:38:33 -0600

    Added functionality to the checkboxes

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ const lengthNumber = document.querySelector('#number');
 range.oninput = function() {
     lengthNumber.textContent = range.value;
 }
+
+// Variables for the checkboxes
+const lowerCheck = document.querySelector('#customCheck1');
+const upperCheck = document.querySelector('#customCheck2');
+const specialCheck = document.querySelector('#customCheck3');
+const numberCheck = document.querySelector('#customCheck4');
+
+// Event listeners for the checkboxes
+lowerCheck.addEventListener('change', isChecked);
+upperCheck.addEventListener('change', isChecked);
+specialCheck.addEventListener('change', isChecked);
+numberCheck.addEventListener('change', isChecked);
+
+// Function to enable the generate password button
+function isChecked() {
+    if (lowerCheck.checked || upperCheck.checked || specialCheck.checked || numberCheck.checked) {
+        document.querySelector('#generateBtn').disabled = "";
+    } else {
+        document.querySelector('#generateBtn').disabled = "disabled";
+    }
+}
