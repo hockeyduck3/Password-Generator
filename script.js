fa59f79dafe54004d72d2c722fcc0c67cef97760
LJ Spencer <hockeyduck3@gmail.com>
2020-03-27 15:51:53 -0600

    Added a new passwordChecker function

--- script.js
+++ script.js
@@ -45,8 +45,7 @@ function passwordMaker() {
 
     const upperSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-    // Special characters selection based off of this website https://docs.oracle.com/cd/E11223_01/doc.910/e11197/app_special_char.htm#MCMAD416
-    const specialSet = `@%+\/'!#$^?:,(){}[]~-_.`;
+    const specialSet = `!@#$%^&*(),.?":{}|<>`;
 
     const numberSet = '0123456789';
 
@@ -77,6 +76,60 @@ function passwordMaker() {
         // Loop through the length of the range and add it to the empty string of userPassword
         for (let i = 0; i < range.value; i++) {
             userPassword += (characters.charAt(Math.floor(Math.random() * characters.length)));
+        }
+
+        // Function to check the password
+        passwordChecker();
+    }
+
+    // This function is to check and make sure that the generated password meets the user's criteria
+    function passwordChecker() {
+        // If the lowercase checkbox is checked
+        if (lowerCheck.checked) {
+            // Check and see if the password contains any lowercase letters
+            if (userPassword.match(/[a-z]/) == null) {
+                console.log(`${userPassword} did not contain any lowercase letters.`);
+                // If the generated password does not contain any lowercase letters then reset the password to an empty string
+                userPassword = '';
+                // Then run the passwordCreation function again
+                passwordCreation();
+            }
+        }
+
+        // If the uppercase checkbox is checked
+        if (upperCheck.checked) {
+            // Check and see if the password contains any uppercase letters
+            if (userPassword.match(/[A-Z]/) == null) {
+                console.log(`${userPassword} did not contain any uppercase letters.`);
+                // If the generated password does not contain any uppercase letters then reset the password to an empty string
+                userPassword = '';
+                // Then run the passwordCreation function again
+                passwordCreation();
+            }
+        }
+
+        // If the special characters checkbox is checked
+        if (specialCheck.checked) {
+            // Check and see if the password contains any uppercase letters
+            if (userPassword.match(/[!@#$%^&*(),.?":{}|<>]/) == null) {
+                console.log(`${userPassword} did not contain any special characters.`);
+                // If the generated password does not contain any special characters then reset the password to an empty string
+                userPassword = '';
+                // Then run the passwordCreation function again
+                passwordCreation();
+            }
+        }
+
+        // If the numbers checkbox is checked
+        if (numberCheck.checked) {
+            // Check and see if the password contains any numbers
+            if (userPassword.match(/[0-9]/) == null) {
+                console.log(`${userPassword} did not contain any numbers.`);
+                // If the generated password does not contain any numbers then reset the password to an empty string
+                userPassword = '';
+                // Then run the passwordCreation function again
+                passwordCreation();
+            }
         }
     }
 }
